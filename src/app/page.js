b7e04cd79c6ac971b8cdc3e87c9bcc26c7340209
Surya-blippi <suryansh.connect@gmail.com>
2025-03-28 16:22:12 +0530

    New Updates

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import Image from 'next/image';
-import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
-import AIFeaturesSection from './components/AIFeaturesSection';
-import FeaturesSection from './components/FeaturesSection';
-import { CTASection } from './components/CTASection'; // Keep this as named import since it works
-import Footer from './components/Footer';
+import { Navbar } from './components/Navbar';
+import { HeroSection } from './components/HeroSection';
+import { AIFeaturesSection } from './components/AIFeaturesSection';
+import { FeaturesSection } from './components/FeaturesSection';
+import { CTASection } from './components/CTASection';
+import { Footer } from './components/Footer';
 
 export default function Home() {
   return (
